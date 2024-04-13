a48d46b3a33b3efce97bcc01f1c6b4a643257359
DarkHole1 <dark.hole1@yandex.ru>
2024-04-13 20:13:44 +0300

    Lesson 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './Components/Footer';
+import TimePage from './Components/TimePage';
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,10 @@
+import './Footer.css';
+function Footer() {
+    return (
+        <p className="Footer">
+            Contact us at <a href='mailto:mail@email.com'>mail@email.com</a>
+        </p>
+    );
+}
+
+export default Footer;
